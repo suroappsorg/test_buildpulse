b0c47cc40bc265922928edcf45486b2190e58f73
Robin Nayathodan <robin.n@browserstack.com>
2021-03-16 11:15:50 +0530

    flaky testadded

--- test/controller_book.js
+++ test/controller_book.js
@@ -6,6 +6,16 @@ describe('BookController', function () {
       const list=Book.list();
       expect(list).to.be.an('array');
     });
+    it('should return a book with isbn', function(){
+      const arr=["9781593275846", "9781449331818", "9781449365035"]
+      const rndNo=Math.floor(Math.random() * arr.length-1) + 0;
+      const book=Book.getISBN(arr[rndNo]);
+
+      expect(book).to.be.an('object');
+      expect(book).to.have.property('isbn', '9781593275846');
+
+
+    });
   });
 
 });
