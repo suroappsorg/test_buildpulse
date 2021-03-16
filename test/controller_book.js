35f8fa80cc328760a1740ba1ebb6b973ac309402
Robin Nayathodan <robin.n@browserstack.com>
2021-03-16 11:21:47 +0530

    positive test cases

--- test/controller_book.js
+++ test/controller_book.js
@@ -8,7 +8,8 @@ describe('BookController', function () {
     });
     it('should return a book with isbn', function(){
       const arr=["9781593275846", "9781449331818", "9781449365035"]
-      const rndNo=Math.floor(Math.random() * arr.length-1) + 0;
+      //const rndNo=Math.floor(Math.random() * arr.length-1) + 0;
+      const rndNo=0;
       const book=Book.getISBN(arr[rndNo]);
 
       expect(book).to.be.an('object');
